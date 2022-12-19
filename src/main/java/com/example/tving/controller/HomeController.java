cdf14acd8743196208eaeb127a384ec0f528553b
lus <lus2530@gmail.com>
2022-12-19 17:42:59 +0900

    web_tving

--- src/main/java/com/example/tving/controller/HomeController.java
+++ src/main/java/com/example/tving/controller/HomeController.java
@@ -1,4 +1,27 @@
 package com.example.tving.controller;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Log4j2
+@Controller
 public class HomeController {
+    @GetMapping("/")
+    public String main(){
+        log.info("----------- home --------------");
+        log.info("----------- 티빙 메인 화면 --------------");
+
+        return "redirect:/home";
+    }
+
+    @GetMapping("/home")
+    public void home(){
+
+    }
+
+    @GetMapping("")
+    public void login(){
+
+    }
 }
---
+++ src/main/java/com/example/tving/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.example.tving.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Log4j2
+@Controller
+@RequestMapping
+public class UserController {
+//    @GetMapping("")
+
+}
